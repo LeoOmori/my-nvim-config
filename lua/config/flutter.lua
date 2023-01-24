31065437cd31cdb793145427ac00ca619ef84554
leoOmori <leonardoomori99@gmail.com>
2023-01-24 16:56:22 -0300

    New flutter lsp config

--- lua/config/flutter.lua
+++ lua/config/flutter.lua
@@ -22,6 +22,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true })
 end
 
 require("flutter-tools").setup {
